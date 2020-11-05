1ae29bfb4a934eeb968220b6ebe19d3d4aedec49
99002774 <72468886+99002774@users.noreply.github.com>
2020-11-05 15:55:54 +0530

    Add files via upload

--- New folder/Client.js
+++ New folder/Client.js
@@ -1,36 +1,50 @@
-const app = require("express")();
-const app2 = require("express")();
-const app3 = require("express")();
-const app4 = require("express")();
+const express = require("express");
+const app = express();
+// const app2 = require("express")();
+// const app3 = require("express")();
+// const app4 = require("express")();
+app.use(express.static('css'));
 
 app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/login.html");
+})
+
+app.get("/login.html", (req, res) => {
+    res.sendFile(__dirname + "/login.html");
+})
+
+
+app.get("/signup.html", (req, res) => {
+    res.sendFile(__dirname + "/signup.html");
+})
+
+
+app.get("/menu.html", (req, res) => {
+    res.sendFile(__dirname + "/Views/menu.html");
+})
+
+app.get("/index.html", (req, res) => {
     res.sendFile(__dirname + "/Views/index.html");
 })
 
-app.listen(1235, () => {
-    console.log("Client App running at 1235");
-})
 
-app2.get("/", (req, res) => {
+app.get("/add.html", (req, res) => {
     res.sendFile(__dirname + "/Views/add.html");
 })
 
-app2.listen(1236, () => {
-    console.log("Client App running at 1236");
-})
 
-app3.get("/", (req, res) => {
+
+app.get("/edit.html", (req, res) => {
     res.sendFile(__dirname + "/Views/edit.html");
 })
 
-app3.listen(1237, () => {
-    console.log("Client App running at 1237");
-})
 
-app4.get("/", (req, res) => {
+
+app.get("/search.html", (req, res) => {
     res.sendFile(__dirname + "/Views/search.html");
 })
 
-app4.listen(1238, () => {
-    console.log("Client App running at 1238");
+
+app.listen(1235, () => {
+    console.log("Client App running at 1235");
 })
